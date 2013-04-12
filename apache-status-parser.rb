46d66fe0223137783ddf337c4aec370eb95ac537
Tyler Rutschman <tyler.rutschman@garmin.com>
2013-04-12 13:54:54 -0500

    Tested on alpha pr, modified for multiple apache controllers

--- apache-status-parser.rb
+++ apache-status-parser.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'pry-debugger'
 
 class ApacheStatusParser
   attr_accessor :keys, :scores, :log_scores
 
   def initialize
-    @buffer = File.open('apachestatus.out','r').read.split("\n").reject(&:empty?)
+    apache_controller = system('which apache2ctl &>/dev/null') ? 'apache2ctl' : 'apachectl'
+    @buffer = `#{apache_controller} status`.split("\n").reject(&:empty?)
     @keys = get_keys
     @range = get_scoreboard_index_range
     @score_line = get_score_line
@@ -30,7 +30,7 @@ class ApacheStatusParser
   def get_scoreboard_index_range
     @range = Array.new
     @range << @buffer.find_index { |e| e.match /currently being processed/ } + 1 
-    @range << @buffer.index("Scoreboard Key:") - 1
+    @range << @buffer.find_index { |e| e.match /Scoreboard Key:/ } - 1 
   end
   def get_score_line
     @buffer[@range[0]..@range[1]].join('')
@@ -46,11 +46,15 @@ class ApacheStatusParser
     @keys.each do |key, value|
       log_string << " #{value}=#{@scores[key]}"
     end
-    log_string
+    File.open('/var/log/metrics/apachestatus.log', 'a') { |f|
+      f.puts log_string
+    }
   end
 end
-parse_data = ApacheStatusParser.new
-parse_data.calculate_scores
-puts parse_data.log_scores
-#File.open('/var/log/metrics/apachestatus.log', 'a') { |f|
-#end
+
+loop do
+  parse_data = ApacheStatusParser.new
+  parse_data.calculate_scores
+  parse_data.log_scores
+  sleep 1 
+end
